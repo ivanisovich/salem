1f931ae4689433e50f597caf269325985353ab0f
ivanisovich <57730983+ivanisovich@users.noreply.github.com>
2024-03-07 22:25:30 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector(".burger-button").addEventListener("click", () => {
+  document.querySelector(".menu-parent").classList.toggle("hidden");
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
